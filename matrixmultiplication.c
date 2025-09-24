95a4ad86c917dcfff4b1a740fa08a29c5407cfc0
shashwat-sharma16 <shashowatt@gmail.com>
2025-09-24 14:16:41 +0530

    Update matrixmultiplication.c

--- matrixmultiplication.c
+++ matrixmultiplication.c
@@ -31,12 +31,6 @@ int main() {
 
     for(i=0; i<m; i++) {
         for(j=0; j<q; j++) {
-            C[i][j] = 0;
-        }
-    }
-
-    for(i=0; i<m; i++) {
-        for(j=0; j<q; j++) {
             for(k=0; k<n; k++) {
                 C[i][j] += A[i][k] * B[k][j];
             }
