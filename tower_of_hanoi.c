e412827dad99ea60580b4f642491123aa34d85c9
shashwat-sharma16 <shashowatt@gmail.com>
2025-11-19 17:20:14 +0530

    Implement Tower of Hanoi solution in C

---
+++ tower_of_hanoi.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+void tower(int n, char s, char h, char d){
+    if(n==0) return;
+    tower(n-1,s,d,h);
+    printf("%c -> %c\n", s,d);
+    tower(n-1,h,s,d);
+    return;
+}
+int main() {
+    int x ;
+    printf("enter number of disks\n ");
+    scanf("%d",&x);
+    tower(x,'A','B','C');
+    return 0;
+}
